d4ab15a311dca8a32a7a9b5a5603b010cc13567f
imvbhargav <87763304+imvbhargav@users.noreply.github.com>
2025-08-31 22:02:05 +0530

    Site url fixed for qf

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://referenceerror.xharv.in",
+  site: "https://quitefragments.xharv.in",
   vite: {
     plugins: [tailwindcss()],
   },

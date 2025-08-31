b3135587004a5f8362be6d5639ea8223b2f7abc1
imvbhargav <87763304+imvbhargav@users.noreply.github.com>
2025-08-31 21:59:30 +0530

    Site url fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://referenceerror.xharv.in",
   vite: {
     plugins: [tailwindcss()],
   },
